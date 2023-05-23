cb662c2f799119f93a446fb2f5e11c7787c92150
Sixtus Nosike <4396551@alu.murciaeduca.es>
2023-05-23 09:17:47 +0200

    Este es el segundo commit del exame

--- ejericiosfiles/src/main/java/com/mycompany/ejericiosfiles/Ejemplo1.java
+++ ejericiosfiles/src/main/java/com/mycompany/ejericiosfiles/Ejemplo1.java
@@ -12,5 +12,6 @@ public class Ejemplo1 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo.");
         System.out.println("Holamundo2");
+        System.out.println("Esto es la primera rama de martesexamengit");
     }
 }
