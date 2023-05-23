a7aad3202462f51563905a280f3b979651b314b6
Sixtus Nosike <4396551@alu.murciaeduca.es>
2023-05-23 09:12:39 +0200

    Este es el segundo commit del examen

--- ejericiosfiles/src/main/java/com/mycompany/ejericiosfiles/Ejemplo1.java
+++ ejericiosfiles/src/main/java/com/mycompany/ejericiosfiles/Ejemplo1.java
@@ -11,5 +11,6 @@ package com.mycompany.ejericiosfiles;
 public class Ejemplo1 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo.");
+        System.out.println("Holamundo2");
     }
 }
